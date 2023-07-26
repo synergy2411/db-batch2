dd3a4cd80b36addb6d8947773864581c072ad80e
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-26 16:01:41 +0530

    more features

--- 02-dom-basics/script/es6-features.js
+++ 02-dom-basics/script/es6-features.js
@@ -136,56 +136,117 @@
 
 // CLASSES
 
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  getDetails() {
-    return this.name;
-  }
-}
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   getDetails() {
+//     return this.name;
+//   }
+// }
 
-class Student extends Person {
-  // Private Member
-  #courseName;
+// class Student extends Person {
+//   // Private Member
+//   #courseName;
 
-  // Static
-  static numberOfStudents = 0;
+//   // Static
+//   static numberOfStudents = 0;
 
-  // no-args constructor
+//   // no-args constructor
 
-  // constructor() { } // CAN't OVERLOAD METHODS
+//   // constructor() { } // CAN't OVERLOAD METHODS
 
-  constructor(studId, studName) {
-    super(studName);
-    this.studId = studId;
-    // this.studName = studName;
-    Student.numberOfStudents++;
-  }
-  // Accessor & Mutators
-  get courseName() {
-    return this.#courseName;
-  }
+//   constructor(studId, studName) {
+//     super(studName);
+//     this.studId = studId;
+//     // this.studName = studName;
+//     Student.numberOfStudents++;
+//   }
+//   // Accessor & Mutators
+//   get courseName() {
+//     return this.#courseName;
+//   }
 
-  set courseName(value) {
-    this.#courseName = value;
-  }
+//   set courseName(value) {
+//     this.#courseName = value;
+//   }
 
-  getDetails() {
-    return `
-    Student ID - ${this.studId} 
-    Student Name - ${super.getDetails()}
-    Course Enrolled = ${this.courseName}
-        `;
-  }
-}
+//   getDetails() {
+//     return `
+//     Student ID - ${this.studId}
+//     Student Name - ${super.getDetails()}
+//     Course Enrolled = ${this.courseName}
+//         `;
+//   }
+// }
 
-let john = new Student("S001", "John Doe");
-john.courseName = "React 101";
-console.log(john.getDetails());
+// let john = new Student("S001", "John Doe");
+// john.courseName = "React 101";
+// console.log(john.getDetails());
 
-let jenny = new Student("S002", "Jenny Public");
-jenny.courseName = "React for Beginners";
-console.log(jenny.getDetails());
+// let jenny = new Student("S002", "Jenny Public");
+// jenny.courseName = "React for Beginners";
+// console.log(jenny.getDetails());
 
-console.log("Total Students : ", Student.numberOfStudents);
+// console.log("Total Students : ", Student.numberOfStudents);
+
+// MAP / SET
+
+// let userOne = {
+//   name: "John",
+// };
+
+// let map = new Map();
+
+// map.set("name", "John");
+// map.set(true, 1);
+// map.set({ age: 32 }, "John");
+
+// console.log("Map Size : ", map.size);
+
+// for (let [key, value] of map.entries()) {
+//   console.log(`${key} : ${value}`);
+// }
+
+// for (let value of map.values()) {
+//   console.log(value);
+// }
+
+// for (let key of map.keys()) {
+//   console.log(key);
+// }
+
+// let set = new Set();
+
+// set.add("John");
+// set.add("Jenny");
+// set.add("Jack");
+// set.add("Jill");
+// set.add("Jill");
+
+// console.log(set.size);
+
+// DEFAULT PARAMETER
+// const demo = (arr = []) => {
+//   if (arr.length > 2) {
+//     console.log("High Values");
+//   } else {
+//     console.log("Low Values");
+//   }
+// };
+
+// demo();
+
+// Short curcuit operator - &&
+
+let arr = [];
+const str = arr.length > 0 ? "Hello" : "Good Bye";
+console.log("String 1 -", str);
+
+const strTwo = arr.length > 0 && "Hello";
+
+console.log("String 2 -", strTwo); // ?
+
+const strThree = null && "Hello";
+
+console.log("String 3 - ", strThree);
