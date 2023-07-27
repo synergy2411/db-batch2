a4c3fd7cd22973ba5255121bd884e417ed29941a
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-27 17:02:35 +0530

    react presentation added

--- blog-app/src/testing/Output.test.tsx
+++ blog-app/src/testing/Output.test.tsx
@@ -1,52 +1,58 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Output from "./Output";
 import userEvent from "@testing-library/user-event";
 
-test("renders heading element", () => {
-  render(<Output />);
+describe("Output Component", () => {
+  test("renders heading element", () => {
+    render(<Output />);
 
-  const headingElement = screen.queryByText(/some demo output/i);
+    const headingElement = screen.queryByText(/some demo output/i);
 
-  expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test("renders 'toggle is false' if button is not clicked", () => {
+    render(<Output />);
+
+    const pElement = screen.getByText("toggle is false", { exact: false });
+
+    expect(pElement).toBeInTheDocument();
+  });
+
+  test("renders not 'toggle is true' when the button is not clicked", () => {
+    render(<Output />);
+
+    const pElement = screen.queryByText(/toggle is true/i);
+
+    expect(pElement).toBeNull();
+  });
+
+  test("renders 'toggle is true' when the button is clicked", async () => {
+    render(<Output />);
+
+    const btnElement = screen.getByRole("button");
+
+    act(() => {
+      userEvent.click(btnElement);
+    });
+    const pElement = await screen.findByText(/toggle is true/i);
+
+    expect(pElement).not.toBeNull();
+  });
+
+  test("renders not 'toggle is false' if the button is clicked", async () => {
+    render(<Output />);
+
+    const btnElement = screen.getByRole("button");
+
+    act(() => {
+      userEvent.click(btnElement);
+    });
+
+    const pElement = await screen.queryByText(/toggle is false/i);
+
+    expect(pElement).not.toBeInTheDocument();
+  });
 });
 
-test("renders 'toggle is false' if button is not clicked", () => {
-  render(<Output />);
-
-  const pElement = screen.getByText("toggle is false", { exact: false });
-
-  expect(pElement).toBeInTheDocument();
-});
-
-test("renders not 'toggle is true' when the button is not clicked", () => {
-  render(<Output />);
-
-  const pElement = screen.queryByText(/toggle is true/i);
-
-  expect(pElement).toBeNull();
-});
-
-test("renders 'toggle is true' when the button is clicked", async () => {
-  render(<Output />);
-
-  const btnElement = screen.getByRole("button");
-
-  userEvent.click(btnElement);
-
-  const pElement = await screen.findByText(/toggle is true/i);
-
-  expect(pElement).not.toBeNull();
-});
-
-test("renders not 'toggle is false' if the button is clicked", async () => {
-  render(<Output />);
-
-  const btnElement = screen.getByRole("button");
-
-  userEvent.click(btnElement);
-
-  const pElement = await screen.findByText(/toggle is false/i);
-
-  expect(pElement).not.toBeInTheDocument();
-});
 // npm run test
