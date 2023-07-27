a16b97e9b45a80d4c43f5f58d6139d6e879ce5de
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-27 11:56:33 +0530

    todo added

---
+++ blog-app/src/components/Todo/AddTodo/AddTodo.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { ITodo } from "../../../model/todo.interface";
+
+interface IProps {
+  addTodo: (todo: ITodo) => void;
+}
+
+const AddTodo: React.FC<IProps> = (props) => {
+  const [enteredLabel, setEnteredLabel] = useState<string>("Hello world");
+
+  const submitHandler = () => {
+    let newTodo: ITodo = {
+      id: Math.round(Math.random() * 1000).toString(),
+      label: enteredLabel,
+    };
+    props.addTodo(newTodo);
+    setEnteredLabel("");
+  };
+  return (
+    <div className="row">
+      <div className="col-6 offset-3">
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-grouo">
+                <div className="row">
+                  <div className="col-8">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="inputLabel"
+                      value={enteredLabel}
+                      onChange={(e) => setEnteredLabel(e.target.value)}
+                    />
+                  </div>
+                  <div className="col-4">
+                    <div className="d-grid">
+                      <button
+                        type="button"
+                        className="btn btn-dark"
+                        onClick={submitHandler}
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodo;
--- blog-app/src/components/Todo/Todo.tsx
+++ blog-app/src/components/Todo/Todo.tsx
@@ -1,25 +1,47 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem/TodoItem";
+import AddTodo from "./AddTodo/AddTodo";
+import { ITodo } from "../../model/todo.interface";
+
+let INITIAL_TODOS: ITodo[] = [
+  { id: "t001", label: "pot the plants" },
+  { id: "t002", label: "renew car insurance" },
+  { id: "t003", label: "buy new jeans" },
+  { id: "t004", label: "shop for grocery" },
+];
 
 const Todo = () => {
-  let todoCollection = [
-    { id: "t001", label: "pot the plants" },
-    { id: "t002", label: "renew car insurance" },
-    { id: "t003", label: "buy new jeans" },
-    { id: "t004", label: "shop for grocery" },
-  ];
+  let [toggle, setToggle] = useState<boolean>(false);
+  const [todoCollection, setTodoCollection] = useState<ITodo[]>(INITIAL_TODOS);
 
   const deleteTodo = (todoId: string) => {
     console.log("About to Delete - ", todoId);
   };
 
+  const addTodo = (newTodo: ITodo) => {
+    setTodoCollection((prevTodos) => [newTodo, ...prevTodos]);
+  };
   return (
     <div>
       <h2 className="text-center">My Todos</h2>
+      <div className="row mb-3">
+        <div className="col-4 offset-4">
+          <div className="d-grid">
+            <button
+              className="btn btn-secondary"
+              onClick={() => setToggle(!toggle)}
+            >
+              {toggle ? "Hide" : "Show"}
+            </button>
+          </div>
+        </div>
+      </div>
+      {toggle && <AddTodo addTodo={addTodo} />}
+
       <div className="row">
-        <TodoItem todo={todoCollection[0]} deleteTodo={deleteTodo} />
-        <TodoItem todo={todoCollection[1]} deleteTodo={deleteTodo} />
-        <TodoItem todo={todoCollection[2]} deleteTodo={deleteTodo} />
-        <TodoItem todo={todoCollection[3]} deleteTodo={deleteTodo} />
+        {todoCollection.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        ))}
       </div>
     </div>
   );
