26cc7f1c000b9e793740addb802b540d2764762f
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-07-27 10:54:00 +0530

    functional prop

--- blog-app/src/components/Todo/Todo.tsx
+++ blog-app/src/components/Todo/Todo.tsx
@@ -8,14 +8,18 @@ const Todo = () => {
     { id: "t004", label: "shop for grocery" },
   ];
 
+  const deleteTodo = (todoId: string) => {
+    console.log("About to Delete - ", todoId);
+  };
+
   return (
     <div>
       <h2 className="text-center">My Todos</h2>
       <div className="row">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        <TodoItem todo={todoCollection[0]} deleteTodo={deleteTodo} />
+        <TodoItem todo={todoCollection[1]} deleteTodo={deleteTodo} />
+        <TodoItem todo={todoCollection[2]} deleteTodo={deleteTodo} />
+        <TodoItem todo={todoCollection[3]} deleteTodo={deleteTodo} />
       </div>
     </div>
   );
--- blog-app/src/components/Todo/TodoItem/TodoItem.tsx
+++ blog-app/src/components/Todo/TodoItem/TodoItem.tsx
@@ -1,12 +1,17 @@
-const TodoItem = () => {
+import React from "react";
+import { ITodo } from "../../../model/todo.interface";
+
+interface IProps {
+  todo: ITodo;
+  deleteTodo: (todoId: string) => void;
+}
+
+const TodoItem: React.FC<IProps> = (props) => {
   return (
     <div className="col-4 my-4">
-      <div className="card">
+      <div className="card" onClick={() => props.deleteTodo(props.todo.id)}>
         <div className="card-header">
-          <h6 className="text-center">
-            {/* {todoCollection[0].label.toUpperCase()} */}
-            TODO ITEM
-          </h6>
+          <h6 className="text-center">{props.todo.label.toUpperCase()}</h6>
         </div>
       </div>
     </div>
